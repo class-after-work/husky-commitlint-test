d4c798bad7466946f9466f834789e6f56de1b660
kyo <qdaszx@naver.com>
2025-02-06 23:52:44 +0900

    build: commitlint fix rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'subject-case': [0],
     'type-enum': [2, 'always', ['feat', 'fix', 'test', 'style', 'refactor', 'perf', 'build', 'ci', 'docs', 'chore', 'etc']],
   },
 };
